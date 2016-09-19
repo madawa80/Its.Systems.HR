fecf144feac0e203330b39fd9b81483f45789d2c
Tommy Larsson <tommy.larsson@adm.umu.se>
2016-09-19 14:17:57 +0200

    Lagt i IDbRepository

---
+++ Domain/Domain/Interfaces/IDbRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Its.Systems.HR.Domain.Interfaces
+{
+    public interface IDbRepository
+    {
+        /// <summary>
+        /// Function to retrieve an IQueryable object
+        /// </summary>
+        /// <typeparam name="T">Type of object</typeparam>
+        /// <returns>IQueryable object</returns>
+        IQueryable<T> Get<T>() where T : class;
+
+        /// <summary>
+        /// Function to get a set of objects
+        /// </summary>
+        /// <typeparam name="T">Type of object</typeparam>
+        /// <returns>set of object of a specific type</returns>
+        DbSet<T> GetSet<T>() where T : class;
+
+        /// <summary>
+        /// Function to add some object to repository
+        /// </summary>
+        /// <typeparam name="T">Type of object</typeparam>
+        /// <param name="input">Domain Object</param>
+        /// <returns>Domain Object</returns>
+        T Add<T>(T input) where T : class;
+        /// <summary>
+        /// Function to delete some object from repository
+        /// </summary>
+        /// <typeparam name="T">Type of object</typeparam>
+        /// <param name="input">Domain Object</param>
+        /// <returns>Boolean</returns>
+        bool Delete<T>(T input) where T : class;
+        /// <summary>
+        /// Function to commit changes to Repository
+        /// </summary>
+        /// <returns>Boolean</returns>
+        bool SaveChanges();
+
+        /// <summary>
+        /// Function for Closeing databaseconnection
+        /// </summary>
+        void Close();
+
+        DbContext Context();
+
+    }
+}
