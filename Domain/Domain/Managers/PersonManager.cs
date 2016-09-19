1fd5cbd10d0a08fe11520031f7bb20f528816d1c
Tommy Larsson <tommy.larsson@adm.umu.se>
2016-09-19 13:55:46 +0200

    setup

---
+++ Domain/Domain/Managers/PersonManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Its.Systems.HR.Domain.Interfaces;
+
+namespace Its.Systems.HR.Domain.Managers
+{
+    public class PersonManager
+    {
+        public IDbRepository db;
+        public PersonManager(IDbRepository repo)
+        {
+            db = repo;
+        }
+
+        //public IQueryable<Person> GetPersons()
+        //{
+        //    db.Get<Person>().Include().Where()
+        //} 
+
+    }
+}
