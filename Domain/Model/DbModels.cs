f4e2bf631f6cfb3bbf8d1344310fe71de7e2e747
Samuel Pettersson <samuel.pettersson@umu.se>
2016-09-19 16:04:02 +0200

    Added db-models to the domain, and dbsets to infrastructure

---
+++ Domain/Model/DbModels.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Its.Systems.HR.Domain.Model
+{
+    public class Activity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual List<Session> Sessions { get; set; }
+
+    }
+    public class Session
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public virtual Location Location { get; set; }
+        [Required]
+        public virtual HrPerson HrPerson { get; set; }
+        [Required]
+        public virtual Activity Activity { get; set; }
+        //public virtual List<Participant> Participants { get; set; }
+
+        //public List<Tag> Tags { get; set; }
+    }
+
+    public class Participant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        //TODO: More properties
+
+        //public virtual List<Session> Sessions { get; set; }
+    }
+
+    public class SessionParticipant
+    {
+        [Key, Column(Order = 0)]
+        public int SessionId { get; set; }
+        [Key, Column(Order = 1)]
+        public int ParticipantId { get; set; }
+        public int Rating { get; set; }
+
+        public virtual Session Session { get; set; }
+        public virtual Participant Participant { get; set; }
+    }
+
+    public class Location
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        //public List<Session> Sessions { get; set; }
+    }
+
+    public class HrPerson
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        //TODO: More properties
+
+        public virtual List<Session> Sessions { get; set; }
+    }
+
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- Infrastructure/HRContext.cs
+++ Infrastructure/HRContext.cs
@@ -5,6 +5,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Its.Systems.HR.Domain.Model;
 
 namespace Its.Systems.HR.Infrastructure
 {
@@ -24,5 +25,12 @@ namespace Its.Systems.HR.Infrastructure
         {
 
         }
+
+        public DbSet<Activity> Activities { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<Participant> Participants { get; set; }
+        public DbSet<SessionParticipant> SessionParticipants { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<HrPerson> HrPersons { get; set; }
     }
 }
