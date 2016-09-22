a504f7c62a0a9acec84d4c45197d61867849d3ed
Abeywickrama Kumbura Gedara <abeywickrama.kumbura.gedara@umu.se>
2016-09-22 15:46:53 +0200

    activitysummary v1

---
+++ Its.Systems.HR.Interface.Web/Controllers/ActivitySummaryController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Its.Systems.HR.Interface.Web.Controllers
+{
+    public class ActivitySummaryController : Controller
+    {
+        // GET: ActivitySummary
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+      
+    }
+}
---
+++ Its.Systems.HR.Interface.Web/ViewModels/ActivitySummaryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Its.Systems.HR.Domain.Model;
+
+namespace Its.Systems.HR.Interface.Web.ViewModels
+{
+    public class ActivitySummaryViewModel
+    {
+        public List<Activity> Activities { get; set; }
+        public List<Session> Sessions { get; set; }
+        public List<SessionParticipant> SessionParticipants { get; set; }
+
+    }
+}
