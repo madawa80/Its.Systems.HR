224939d73a7cf8cf07605744857924de5f0e14eb
Samuel Pettersson <samuel.pettersson@umu.se>
2016-09-26 14:00:41 +0200

    Participant Overview now async

--- Its.Systems.HR.Interface.Web/Controllers/ParticipantController.cs
+++ Its.Systems.HR.Interface.Web/Controllers/ParticipantController.cs
@@ -62,13 +62,13 @@ namespace Its.Systems.HR.Interface.Web.Controllers
         [HttpPost]
         public ActionResult SaveComments(int personId, string comments)
         {
-            var result = new { Success = "True" };
+            var result = new { Success = true };
 
             if (_personManager.SaveCommentsForParticipant(personId, comments))
                 return Json(result, JsonRequestBehavior.AllowGet);
 
             // TODO: ErrorMessage
-            result = new { Success = "False" };
+            result = new { Success = false };
             return Json(result, JsonRequestBehavior.AllowGet);
             //return RedirectToAction("Details", new { id = personId });
         }
@@ -76,37 +76,68 @@ namespace Its.Systems.HR.Interface.Web.Controllers
         [HttpPost]
         public ActionResult SaveWishes(int personId, string wishes)
         {
-            var result = new { Success = "True" };
+            var result = new { Success = true };
 
             if (_personManager.SaveWishesForParticipant(personId, wishes))
                 return Json(result, JsonRequestBehavior.AllowGet);
 
-            result = new { Success = "False" };
+            result = new { Success = false };
             return Json(result, JsonRequestBehavior.AllowGet);
         }
 
         [HttpPost]
         public ActionResult AddPersonToSession(int sessionId, int personId)
         {
-            var result = new { Success = true, ErrorMessage = ""};
+            var session = _activityManager.GetSessionById(sessionId);
+
+            var result = new
+            {
+                Success = true,
+                ErrorMessage = "",
+                PersonId = personId,
+                SessionId = sessionId,
+                SessionName = session.Name,
+                Year = session.StartDate.Year,
+                Month = session.StartDate.Month,
+                Day = session.StartDate.Day,
+            };
 
             if (!_activityManager.AddParticipantToSession(personId, sessionId))
-                result = new { Success = false, ErrorMessage = "Personen är redan registrerad på kurstillfället."};
+                result = new { Success = false, ErrorMessage = "Personen är redan registrerad på kurstillfället.",
+                    PersonId = 0, SessionId = 0, SessionName = "", Year = 0, Month = 0, Day = 0};
 
             return Json(result, JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult RemovePersonFromSession(int sessionId, int personId)
         {
-            var result = new { Success = "True" };
+            var result = new { Success = true };
 
             //if (_personManager.GetParticipantById(personId) == null || _activityManager.GetSessionById(sessionId) == null)
             //    result = new { Success = "Fail" };
 
             if (!_activityManager.RemoveParticipantFromSession(personId, sessionId))
-                result = new { Success = "False" };
+                result = new { Success = false };
 
             return Json(result, JsonRequestBehavior.AllowGet);
         }
+
+        public ActionResult ParticipantStatisticSummary(int personid)
+        {
+            var allParticipantSessions = _activityManager.GetAllSessionsForParticipantById(personid).ToList();
+            var result = new ParticipantStatisticSummaryViewModel()
+            {
+                TotalCount = allParticipantSessions.Count,
+                CountThisYear = allParticipantSessions.Count(n => n.StartDate.Year == DateTime.Now.Year)
+            };
+
+            return PartialView("_ParticipantStatisticSummaryPartial", result);
+        }
+    }
+
+    public class ParticipantStatisticSummaryViewModel
+    {
+        public int TotalCount { get; set; }
+        public int CountThisYear { get; set; }
     }
 }
