26260008aaeec46e35bf628af739c86ad113d742
Abeywickrama Kumbura Gedara <abeywickrama.kumbura.gedara@umu.se>
2016-09-29 15:59:42 +0200

    stst summary v.1

---
+++ Its.Systems.HR.Interface.Web/Controllers/SessionSummaryStatisticsController.cs
@@ -0,0 +1,37 @@
+﻿using Its.Systems.HR.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Its.Systems.HR.Domain.Model;
+
+namespace Its.Systems.HR.Interface.Web.Controllers
+{
+
+
+    public class SessionSummaryStatisticsController : Controller
+    {
+
+        private IActivityManager _activityManager;
+        private IPersonManager _personManager;
+
+        public SessionSummaryStatisticsController(IActivityManager manager, IPersonManager personManager)
+        {
+            _activityManager = manager;
+            _personManager = personManager;
+        }
+
+        public ActionResult Details(int? year)
+        {
+            Session session;
+
+            
+
+            return View();
+
+        }
+
+    }
+
+}
