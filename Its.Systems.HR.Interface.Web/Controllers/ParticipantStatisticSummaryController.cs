dac220847fdd05546688639cea0abd3a03385554
Samuel Pettersson <samuel.pettersson@umu.se>
2016-10-04 10:36:44 +0200

    Bugfixes participant overview, filtersessions

--- Its.Systems.HR.Interface.Web/Controllers/ParticipantStatisticSummaryController.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Its.Systems.HR.Interface.Web.Controllers
-{
-    public class ParticipantStatisticSummaryController
-    {
-        public int TotalCount { get; set; }
-        public int CountThisYear { get; set; }
-    }
-}
